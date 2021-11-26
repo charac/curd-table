0ec2692bd645e3a0d0f8a6f516e20f034839aa99
236604099@qq.com <236604099@qq.com>
2021-11-26 18:28:57 +0800

    添加element-ui组件

--- examples/main.js
+++ examples/main.js
@@ -1,7 +1,18 @@
+/*
+ * @since: 2021-11-24 14:23:10
+ * @LastAuthor: Do not edit
+ * @lastTime: 2021-11-26 18:28:35
+ * @文件相对于项目的路径: \financial-sx-org-webe:\npm仓库\curd-table\examples\main.js
+ * @Author: ltm@xtoneict.com
+ * @message: 
+ */
 import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
-
-createApp(App).use(store).use(router).mount('#app')
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+const app = createApp(App);
+// Vue.config.productionTip = false
+app.use(ElementUI).use(store).use(router).mount('#app')
